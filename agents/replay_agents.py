52705f606ba1b8fb15eb675f31d35816f3e00e13
saujasv <saujas1999@gmail.com>
2025-02-19 23:23:58 -0500

    replay agents tested

--- agents/replay_agents.py
+++ agents/replay_agents.py
@@ -1,30 +1,28 @@
-from mrcad.action import Drawing, Instruction, Execution
-from mrcad.design import Design
+from typing import List, Tuple
+from mrcad import Design, Drawing, Instruction, Execution, Action
 from mrcad.agents import AbstractDesignerAgent, AbstractMakerAgent
-from mrcad.action import Action
 from mrcad.env_utils import Role
 
 
 class ReplayDesigner(AbstractDesignerAgent):
-    def __init__(self, turns: list[tuple[str, Drawing]]):
+    def __init__(self, turns: List[Tuple[Design, Action]]):
         self.turns = turns
         self.turn_idx = 0
 
-    def act(self, target: Design, conversation_history: list[tuple[Design, Action]]):
-        text, drawing = self.turns[self.turn_idx]
+    def act(
+        self, target: Design, conversation_history: List[Tuple[Design, Action]]
+    ) -> Action:
+        context, instruction = self.turns[self.turn_idx]
         self.turn_idx += 1
-        return Instruction(
-            text=text if isinstance(text, str) else None,
-            drawing=Drawing(drawing=drawing),
-        )
+        return instruction
 
 
 class ReplayMaker(AbstractMakerAgent):
-    def __init__(self, turns: list[Design]):
+    def __init__(self, turns: List[Tuple[Design, Action]]):
         self.turns = turns
         self.turn_idx = 0
 
-    def act(self, conversation_history: list[tuple[Design, Action]]):
-        action = self.turns[self.turn_idx]
+    def act(self, conversation_history: List[Tuple[Design, Action]]) -> Action:
+        context, execution = self.turns[self.turn_idx]
         self.turn_idx += 1
-        return Execution(design=action)
+        return execution
